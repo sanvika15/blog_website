0ed802bf2e2ffa944b2ec8c1fe7808b4a7523dc0
sanvika15 <137372400+sanvika15@users.noreply.github.com>
2024-05-12 20:04:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
 const mongoose = require('mongoose')
-const app express()
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/bharatInternDatabase')
 
 app.set('view engine', 'ejs')
-app.get('/',(req, res)=>{
-    res.send("Welcome to project")
-    })
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
 
 app.listen(3000)
